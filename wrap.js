0d29ff89d2ed62a2e0af87ca451b7d3f7a40bd7e
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-06-27 10:29:56 -0500

    pass first test case

---
+++ wrap.js
@@ -0,0 +1,13 @@
+// let sent =
+//   'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.';
+
+function wrap(str, num) {
+  let newString = '';
+  if (str === '') {
+    return newString;
+  }
+  return 'some other string';
+}
+
+//wrap(sent, 20);
+module.exports = wrap;
